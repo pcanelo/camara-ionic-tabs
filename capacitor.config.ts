63803272164ca441f8649afd04ccb85b0936dc04
Patricio Canelo <pcanelo@gmail.com>
2023-05-19 17:50:18 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'camara-ionic-tabs',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
